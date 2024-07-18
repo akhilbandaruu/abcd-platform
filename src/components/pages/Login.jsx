a402576d404adcb0dc6a51934143fbfec814863b
VSH Kiran <saiharikiran@outlook.com>
2024-07-18 13:28:44 +0530

    added backend code for authentication

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,79 +1,102 @@
 // src/Login.js
-import React, { useState } from 'react';
-import '../styles/Login.css';
-import logo from '../../assets/logos/badge-abcd-logo.svg';
-import SocialCommunities from '../sidebar/social-communities/Social-communities';
+import React, { useState } from "react";
+import "../styles/Login.css";
+import logo from "../../assets/logos/badge-abcd-logo.svg";
+// import SocialCommunities from "../sidebar/social-communities/Social-communities";
 
 function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    // Handle login logic here
-    console.log('Email:', email);
-    console.log('Password:', password);
+
+    const data = {
+      email: email,
+      password: password,
+    };
+
+    const response = await fetch("http://localhost:3002/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    const result = await response.json();
+    setError(result.message);
+    if (result.success) {
+      localStorage.setItem("token", result.token);
+      window.location.replace("/");
+    }
   };
 
   return (
-    <div className='login-container'>
-      <div className='logo badge-abcd-logo'>
-        <img src={logo} alt='ABCD Logo' />
-      </div>
-      <h1>Welcome Back</h1>
-      <p>For secure access to your account, please enter your credentials.</p>
-      <form onSubmit={handleSubmit}>
-        <div className='form-group'>
-          <input
-            type='email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            placeholder='Email Address'
-          />
+    <div className="main-container">
+      <div className="login-container">
+        <div className="logo badge-abcd-logo">
+          <img src={logo} alt="ABCD Logo" />
         </div>
-        <div className='form-group'>
-          <input
-            type='password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            placeholder='Password'
-          />
-        </div>
-        <button type='submit' className='btn'>
-          Login
-        </button>
-      </form>
-      <div className='formlink-below'>
-        <ul className='formlinks'>
-          <li>
-            <a href='/' className='orange-link'>
-              Create Accounts
-            </a>
-          </li>
-          <li>
-            <a href='/' className='orange-link'>
-              Forget Password?
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div className='social-login'>
-        <div className='or-divider'>
-          <ul className='or-divider-listitems'>
-            <li className='or-divider-listitem'>
-              <hr />
+        <h1>Welcome Back</h1>
+        <p>For secure access to your account, please enter your credentials.</p>
+        <p>{error}</p>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <input
+              name="email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              placeholder="Email Address"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              name="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              placeholder="Password"
+            />
+          </div>
+          <button type="submit" className="btn">
+            Login
+          </button>
+        </form>
+        <div className="formlink-below">
+          <ul className="formlinks">
+            <li>
+              <a href="/register" className="orange-link">
+                Create Account
+              </a>
             </li>
-            <li className='or-divider-listitem'>
-              <span className='or-divider-line'>OR</span>
-            </li>
-            <li className='or-divider-listitem'>
-              <hr />
+            <li>
+              <a href="/" className="orange-link">
+                Forget Password?
+              </a>
             </li>
           </ul>
         </div>
-        <SocialCommunities />
+        <div className="social-login">
+          <div className="or-divider">
+            <ul className="or-divider-listitems">
+              <li className="or-divider-listitem">
+                <hr />
+              </li>
+              <li className="or-divider-listitem">
+                <span className="or-divider-line">OR</span>
+              </li>
+              <li className="or-divider-listitem">
+                <hr />
+              </li>
+            </ul>
+          </div>
+          {/* <SocialCommunities /> */}
+        </div>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,64 +1,75 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './App';
-import Body from './components/body/Body';
-import Projects from './components/pages/Projects';
-import Files from './components/pages/Files';
-import Messages from './components/pages/Messages';
-import Payments from './components/pages/Payments';
-import Reports from './components/pages/Reports';
-import TicketSystem from './components/pages/TicketSystem';
-import Settings from './components/pages/Settings';
-import Profile from './components/pages/Profile';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import Body from "./components/body/Body";
+import Projects from "./components/pages/Projects";
+import Files from "./components/pages/Files";
+import Messages from "./components/pages/Messages";
+import Payments from "./components/pages/Payments";
+import Reports from "./components/pages/Reports";
+import TicketSystem from "./components/pages/TicketSystem";
+import Settings from "./components/pages/Settings";
+import Profile from "./components/pages/Profile";
+import Login from "./components/pages/Login";
+import Register from "./components/pages/Register";
+import PrivateRoute from "./components/body/private-route";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <App />,
+    path: "/",
+    element: <PrivateRoute element={App} />,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <Body />,
       },
       {
-        path: '/projects',
+        path: "/projects",
         element: <Projects />,
       },
       {
-        path: '/files',
+        path: "/files",
         element: <Files />,
       },
       {
-        path: '/messages',
+        path: "/messages",
         element: <Messages />,
       },
       {
-        path: '/payments',
+        path: "/payments",
         element: <Payments />,
       },
       {
-        path: '/reports',
+        path: "/reports",
         element: <Reports />,
       },
       {
-        path: '/support-ticket',
+        path: "/support-ticket",
         element: <TicketSystem />,
       },
       {
-        path: '/settings',
+        path: "/settings",
         element: <Settings />,
       },
       {
-        path: '/user-profile',
+        path: "/user-profile",
         element: <Profile />,
       },
     ],
   },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
